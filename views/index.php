17414b1d3f3823d3985d866277c662146f1540a9
Erik Uus <erik.uus@gmail.com>
2023-10-08 13:39:10 +0300

    Muudab struktuuri

--- views/index.php
+++ views/index.php
@@ -7,30 +7,6 @@
 <body>
 	<div class="flex space-x-4">
 		<div class="flex-1 bg-gray-light-100 p-4">
-			<h1>Utilities</h1>
-			<?php foreach(array_diff(scandir('./utilities'), array('..', '.')) as $file): ?>
-				<a target="_blank" class="block mx-10 my-1 text-blue-500" href="./utilities/<?php echo $file ?>">
-					<?php echo $file ?>
-				</a>
-			<?php endforeach; ?>
-		</div>
-		<div class="flex-1 bg-gray-light-100 p-4">
-			<h1>Elements</h1>
-			<?php foreach(array_diff(scandir('./elements'), array('..', '.')) as $file): ?>
-				<a target="_blank" class="block mx-10 my-1 text-blue-500" href="./elements/<?php echo $file ?>">
-					<?php echo $file ?>
-				</a>
-			<?php endforeach; ?>
-		</div>
-		<div class="flex-1 bg-gray-light-100 p-4">
-			<h1>Forms</h1>
-			<?php foreach(array_diff(scandir('./forms'), array('..', '.')) as $file): ?>
-				<a target="_blank" class="block mx-10 my-1 text-blue-500" href="./forms/<?php echo $file ?>">
-					<?php echo $file ?>
-				</a>
-			<?php endforeach; ?>
-		</div>
-		<div class="flex-1 bg-gray-light-100 p-4">
 			<h1>Layouts</h1>
 			<?php foreach(array_diff(scandir('./layouts'), array('..', '.')) as $file): ?>
 				<a target="_blank" class="block mx-10 my-1 text-blue-500" href="./layouts/<?php echo $file ?>">
@@ -39,8 +15,32 @@
 			<?php endforeach; ?>
 		</div>
 		<div class="flex-1 bg-gray-light-100 p-4">
-			<h1>Navigation</h1>
-			<?php foreach(array_diff(scandir('./navigation'), array('..', '.')) as $file): ?>
+			<h1>Pages</h1>
+			<?php foreach(array_diff(scandir('./pages'), array('..', '.')) as $file): ?>
+				<a target="_blank" class="block mx-10 my-1 text-blue-500" href="./utilities/<?php echo $file ?>">
+					<?php echo $file ?>
+				</a>
+			<?php endforeach; ?>
+		</div>
+		<div class="flex-1 bg-gray-light-100 p-4">
+			<h1>Portlets</h1>
+			<?php foreach(array_diff(scandir('./portlets'), array('..', '.')) as $file): ?>
+				<a target="_blank" class="block mx-10 my-1 text-blue-500" href="./forms/<?php echo $file ?>">
+					<?php echo $file ?>
+				</a>
+			<?php endforeach; ?>
+		</div>
+		<div class="flex-1 bg-gray-light-100 p-4">
+			<h1>Components</h1>
+			<?php foreach(array_diff(scandir('./components'), array('..', '.')) as $file): ?>
+				<a target="_blank" class="block mx-10 my-1 text-blue-500" href="./elements/<?php echo $file ?>">
+					<?php echo $file ?>
+				</a>
+			<?php endforeach; ?>
+		</div>
+		<div class="flex-1 bg-gray-light-100 p-4">
+			<h1>Experiments</h1>
+			<?php foreach(array_diff(scandir('./experiments'), array('..', '.')) as $file): ?>
 				<a target="_blank" class="block mx-10 my-1 text-blue-500" href="./navigation/<?php echo $file ?>">
 					<?php echo $file ?>
 				</a>
